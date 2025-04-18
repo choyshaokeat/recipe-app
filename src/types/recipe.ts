8ffa08d23846cd7675e2b5eca14a70d2d5f42ac7
choy shao keat <shaokeat.choy@hytechc.com>
2025-04-18 11:58:19 +0800

    feat: update type

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,1 +1,23 @@
-export type Recipe = Record<string, any>;
+interface Author {
+  name: string;
+}
+interface RecipeBase {
+  name: string;
+  url: string;
+  image: string[] | string;
+  author: Author;
+  datePublished: string;
+  dateModified: string;
+  description: string;
+  prepTime: string;
+  cookTime: string;
+  totalTime: string;
+  keywords: string;
+  recipeCuisine: string;
+  recipeCategory: string;
+  recipeIngredient: string[];
+  recipeInstructions: string[] | string;
+  [key: string]: any /* to cater other fields */;
+}
+
+export type Recipe = RecipeBase[];
